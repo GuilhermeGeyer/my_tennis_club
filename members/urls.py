f08712a65879e47d669ff2795beaf1d8f4221a82
GuilhermeGeyer <98240700+GuilhermeGeyer@users.noreply.github.com>
2023-11-28 21:41:32 -0300

    Added files.

---
+++ members/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 27 14:36:10 2023
+
+@author: user
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('members/', views.members, name='members'),
+]
