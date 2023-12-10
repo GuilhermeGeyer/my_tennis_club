2e175765346fb4cbf5aa2457de83272ed4c10021
Guilherme Geyer <ghiper646@yahoo.com.br>
2023-12-10 16:11:40 -0300

    Added variables and admin.

--- members/views.py
+++ members/views.py
@@ -7,7 +7,7 @@ def members(request):
     mymembers = Member.objects.all().values()
     template = loader.get_template('all_members.html')
     context = {
-        'mymembers': mymembers
+        'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
 
@@ -27,8 +27,11 @@ def main(request):
 
 
 def testing(request):
+    mymembers = Member.objects.all().values()
     template = loader.get_template('template.html')
     context = {
         'fruits': ['Apple', 'Banana', 'Cherry'],
+        'firstname': 'Linus',
+        'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
